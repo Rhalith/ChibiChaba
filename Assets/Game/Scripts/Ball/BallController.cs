8450340e2aa9b41fd491c0d23d688f125264f429
Rhalith <akmannuhyigit@gmail.com>
2024-02-17 23:56:23 +0300

    Add: ball and brick controller

---
+++ Assets/Game/Scripts/Ball/BallController.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+
+namespace Ball
+{
+    public class BallController : MonoBehaviour
+    {
+        public float initialSpeed = 25f;
+        private Rigidbody2D rb;
+        private Vector2 lastVelocity;
+
+        private void Start()
+        {
+            rb = GetComponent<Rigidbody2D>();
+            rb.velocity = new Vector2(initialSpeed, initialSpeed);
+        }
+
+        private void Update()
+        {
+            lastVelocity = rb.velocity;
+        }
+
+        private void OnCollisionEnter2D(Collision2D collision)
+        {
+            var speed = lastVelocity.magnitude;
+            var direction = Vector2.Reflect(lastVelocity.normalized, collision.contacts[0].normal);
+            rb.velocity = direction * Mathf.Max(speed, initialSpeed);
+        }
+    }
+}
---
+++ Assets/Game/Scripts/Brick/BrickController.cs
@@ -0,0 +1,19 @@
+﻿using Essentials;
+using UnityEngine;
+
+namespace Brick
+{
+    public class BrickController : MonoBehaviour
+    {
+        private int _points = 100;
+
+        private void OnCollisionEnter2D(Collision2D collision)
+        {
+            if (collision.gameObject.CompareTag("Ball"))
+            {
+                GameManager.Instance.BrickDestroyed(_points);
+                Destroy(gameObject);
+            }
+        }
+    }
+}
