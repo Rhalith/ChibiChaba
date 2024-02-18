c13e53b36319d337ecf04217c8fcc026a8630250
Rhalith <akmannuhyigit@gmail.com>
2024-02-18 16:57:10 +0300

    Add: responsive camera

---
+++ Assets/Game/Scripts/Essentials/ResponsiveCamera.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+
+namespace Essentials
+{
+    public class ResponsiveCamera : MonoBehaviour
+    {
+        [SerializeField] private Transform levelBounds;
+
+        private void Awake()
+        {
+            AdjustCameraToFitLevelBounds();
+        }
+
+        private void AdjustCameraToFitLevelBounds()
+        {
+            if (levelBounds == null) return;
+
+            var levelBoundsSize = levelBounds.GetComponent<SpriteRenderer>().bounds.size;
+
+            float orthographicSize = levelBoundsSize.y / 2;
+
+            float horizontalSize = (levelBoundsSize.x / 2) / Camera.main.aspect;
+
+            Camera.main.orthographicSize = Mathf.Max(orthographicSize, horizontalSize);
+
+            CenterCameraOnLevelBounds();
+        }
+
+        private void CenterCameraOnLevelBounds()
+        {
+            if (levelBounds == null) return;
+
+            Vector3 levelBoundsCenter = levelBounds.GetComponent<SpriteRenderer>().bounds.center;
+            Camera.main.transform.position = new Vector3(levelBoundsCenter.x, levelBoundsCenter.y, Camera.main.transform.position.z);
+        }
+    }
+}
