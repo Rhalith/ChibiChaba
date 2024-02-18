63852c45be0bdb1f0372eef06cf953c1c9a43d60
Rhalith <akmannuhyigit@gmail.com>
2024-02-18 17:32:59 +0300

    Add: videos and video player positioner

---
+++ Assets/Game/Scripts/VideoPlayer/VideoPlayerPositioner.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+namespace VideoPlayer
+{
+    public class VideoPlayerPositioner : MonoBehaviour
+    {
+        private void Start()
+        {
+            PositionVideoPlayer();
+        }
+
+        private void PositionVideoPlayer()
+        {
+            Vector3 bottomCenter = new Vector3(0.5f, 0.0f, Camera.main.nearClipPlane);
+            
+            Vector3 worldPosition = Camera.main.ViewportToWorldPoint(bottomCenter);
+        
+            worldPosition.y += transform.lossyScale.y / 2;
+            transform.position = new Vector3(worldPosition.x, worldPosition.y, transform.position.z);
+        }
+    }
+}
