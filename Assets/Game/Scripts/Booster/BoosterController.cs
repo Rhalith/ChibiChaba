267ca9f545ad5a9eefce57ffc1cbe8cbc01f0a80
Rhalith <akmannuhyigit@gmail.com>
2024-02-24 15:17:42 +0300

    Change: booster speed

--- Assets/Game/Scripts/Booster/BoosterController.cs
+++ Assets/Game/Scripts/Booster/BoosterController.cs
@@ -1,10 +1,18 @@
-﻿using UnityEngine;
+﻿using System;
+using UnityEngine;
 
 namespace Booster
 {
     public class BoosterController : MonoBehaviour
     {
         [SerializeField] private BoosterType boosterType;
+        [SerializeField] private float speed = 10.0f;
         public BoosterType Type => boosterType;
+
+        private void OnEnable()
+        {
+            Rigidbody2D rb = GetComponent<Rigidbody2D>();
+            rb.velocity = new Vector2(0, -speed);
+        }
     }
 }
